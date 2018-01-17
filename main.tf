33260fdb622ec74c0860d7762bfd2eca7d9ca3b3
Luis Gallardo <lgallardo@edrans.com>
2018-01-17 17:02:18 -0300

    Changed name for name_prefix to avoid conflicts

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@
  */
 
 resource "aws_launch_configuration" "launch_config" {
-  name                 = "${var.lc_name}"
+  name_prefix          = "${var.lc_name}-"
   image_id             = "${var.ami_id}"
   instance_type        = "${var.instance_type}"
   iam_instance_profile = "${var.iam_instance_profile}"
