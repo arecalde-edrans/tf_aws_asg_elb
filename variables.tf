7718dc19d0c65b9adc12d5af04691b8257f6eb53
arecalde-contractor <arecalde-contractor@zappos.com>
2018-03-30 10:57:52 -0300

    Defined variable create

--- variables.tf
+++ variables.tf
@@ -5,6 +5,10 @@
 #
 # Launch Configuration Variables
 #
+variable "create" {
+  description = "Set to true if you want to create this resource(Used for optional use cases)"
+}
+
 variable "lc_name" {}
 
 variable "ami_id" {
